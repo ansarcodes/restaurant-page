25a817178514dfd9e5421638f7d35574e93c6cdc
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2026-06-07 12:58:13 +0500

    Append rework

--- src/home.js
+++ src/home.js
@@ -4,7 +4,6 @@ export default function() {
     let divTitle = document.createElement("div");
     divTitle.textContent = "Mayweed  Valley";
     divTitle.classList.add("title");
-    divContent.appendChild(divTitle);
     
     let homeImageFigure = document.createElement("figure");
     let homeImageFigcaption = document.createElement("figcaption");
@@ -13,33 +12,28 @@ export default function() {
     homeImage.src = homeImg;
     homeImage.alt = "Cherry Pie";
     homeImageFigcaption.innerHTML = `Photo by <a href="https://unsplash.com/@shamblenstudios?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Shamblen Studios</a> on <a href="https://unsplash.com/photos/a-bunch-of-different-types-of-fruit-k0u3zcxTIaY?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>`;
-    homeImageFigure.appendChild(homeImage);
-    homeImageFigure.appendChild(homeImageFigcaption);
-    divContent.appendChild(homeImageFigure);
+    homeImageFigure.append(homeImage, homeImageFigcaption);
 
     let homeDescription = document.createElement("div");
     homeDescription.classList.add("description");
     homeDescription.textContent = "Welcome to our cozy place! We offer tasty food and inner peace.";
-    divContent.appendChild(homeDescription);
 
     let homeSchedule = document.createElement("div");
     homeSchedule.classList.add("schedule");
     let scheduleTitle = document.createElement("div");
     scheduleTitle.classList.add("schedule-title");
     scheduleTitle.textContent = "Schedule";
-    homeSchedule.appendChild(scheduleTitle);
     let schedule1 = document.createElement("div");
     schedule1.classList.add("schedule-days");
     schedule1.textContent = "Mon-Fri 09:00-18:00";
-    homeSchedule.appendChild(schedule1);
     let schedule2 = document.createElement("div");
     schedule2.classList.add("schedule-days");
     schedule2.textContent = "Sat-Sun 10:00-15:00"
-    homeSchedule.appendChild(schedule2);
-    divContent.appendChild(homeSchedule);
+    homeSchedule.append(scheduleTitle, schedule1, schedule2);
 
     let homeAddress = document.createElement("div");
     homeAddress.classList.add("address");
     homeAddress.textContent = "Address: 42 Green Ave, Mayweed Valley";
-    divContent.appendChild(homeAddress);
+
+    divContent.append(divTitle, homeImageFigure, homeDescription, homeSchedule, homeAddress);
 };
