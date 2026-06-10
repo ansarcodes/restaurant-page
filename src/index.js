bea672fe2b585ba33c41107d786b63c550a0ac1b
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2026-06-10 14:22:07 +0500

    Add page change on button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import "./styles.css";
 import homePage from "./home.js";
 import menuPage from "./menu.js";
-// homePage();
-menuPage();
+
+homePage();
+// menuPage();
+//aboutPage();
+
+const divContent = document.getElementById("content");
+
+document.getElementById("home-btn").addEventListener("click", () => {
+    divContent.replaceChildren();
+    homePage();
+})
+document.getElementById("menu-btn").addEventListener("click", () => {
+    divContent.replaceChildren();
+    menuPage();
+})
+document.getElementById("about-btn").addEventListener("click", () => {
+    divContent.replaceChildren();
+    aboutPage();
+})
