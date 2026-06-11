87968d4d126a0af3ecb65f95fc19d9b0db933302
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2026-06-11 15:42:12 +0500

    Add beverages

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,7 @@
 import cherryPieImg from "./images/cherry-pie.jpg";
 import blinyImg from "./images/bliny.jpg";
 import carrotCakeImg from "./images/carrot-cake.jpg";
+import beveragesImg from "./images/beverages.jpg";
 
 export default function() {
     const divContent = document.getElementById("content");
@@ -17,6 +18,7 @@ export default function() {
         },
         {name: "Bliny", description: "A traditional slavic thin pancakes", price: "$25", image:blinyImg, credits: `Photo by <a href="https://unsplash.com/@nouvellebeautybymarion?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">M Draa</a> on <a href="https://unsplash.com/photos/white-and-blue-ceramic-plate-26NcU5Vkd5o?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>`},
         {name: "Carrot Cake", description:"A sweet and moist spice cake, full of cut carrots and toasted nuts, and covered in cream cheese icing", price: "$50", image: carrotCakeImg, credits: `Photo by <a href="https://unsplash.com/@wanderlust612?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Katelynn Ruffner</a> on <a href="https://unsplash.com/photos/white-pasta-on-black-ceramic-plate-A43QCRZK7NM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>`},
+        {name: "Beverages", description: "Tea, coffee, milk, water", price: "Free with any order", image:beveragesImg, credits: `Photo by <a href="https://unsplash.com/@caferhino?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Cafe Rhino</a> on <a href="https://unsplash.com/photos/white-round-plastic-cups-on-gray-concrete-floor-94dk5XdeSHA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>`}
     ];
 
     (function(){
