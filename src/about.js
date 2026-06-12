ef38127fc5b1bb9276d47e4c8ba6f7b9f4c0357c
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2026-06-12 11:13:12 +0500

    Fill out about page

--- src/about.js
+++ src/about.js
@@ -1,4 +1,20 @@
 import divContent from "./index.js";
 export default function(){
-    
+    let aboutTitle = document.createElement("div");
+    aboutTitle.classList.add("title");
+    aboutTitle.textContent = "About Us";
+
+    let aboutDescription = document.createElement("div");
+    aboutDescription.classList.add("about-text");
+    aboutDescription.textContent = "We are a small cozy place in a Mayweed Valley.";
+
+    let aboutEstablished = document.createElement("div");
+    aboutEstablished.classList.add("about-text");
+    aboutEstablished.textContent = "Est. 2004";
+
+    let aboutContact = document.createElement("div");
+    aboutContact.classList.add("about-text");
+    aboutContact.textContent = "Contact Us: +1-234-56-78 | contact@email.com";
+
+    divContent(aboutTitle, aboutDescription, aboutEstablished, aboutContact);
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import homePage from "./home.js";
 import menuPage from "./menu.js";
+import aboutPage from "./about.js";
 
 homePage();
 // menuPage();
